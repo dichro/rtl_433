41a675f09c080fc45e747adf4922e41e3a350b4a
Miki Habryn <dichro@rcpt.to>
2017-10-10 12:00:00 -0700

    update count and temperature gauge metrics

--- src/prometheus.cc
+++ src/prometheus.cc
@@ -11,18 +11,37 @@ int main(int argc, char **argv) {
 namespace {
 using namespace prometheus;
 
-class Exporter {
+template <typename T> class AutoMetrics {
 public:
-    Exporter(std::string);
-    Gauge& temperature(std::map<std::string, std::string> labels);
-
+    AutoMetrics(Family<T>* family);
+    T& get(std::string key, std::map<std::string, std::string>& labels);
 private:
+    Family<T> *family_;
+    std::map<std::string, T*> metrics_;
+};
+
+template <typename T>
+AutoMetrics<T>::AutoMetrics(Family<T>* family) : family_(family) {}
+
+template <typename T>
+T& AutoMetrics<T>::get(std::string key, std::map<std::string, std::string>& labels) {
+    auto search = metrics_.find(key);
+    if (search != metrics_.end()) {
+        return *search->second;
+    }
+    T& t = family_->Add(labels);
+    metrics_.insert(std::make_pair(key, &t));
+    return t;
+}
+
+struct Exporter {
+    Exporter(std::string);
+
     Exposer* exposer_;
     std::shared_ptr<Registry> registry_;
 
-    Family<Gauge> *temperature_family_;
-    // TODO(dichro): why can't I retrieve metrics from a Family?
-    std::map<std::string, Gauge*> temperatures_;
+    AutoMetrics<Counter> updates_;
+    AutoMetrics<Gauge> temperature_;
 };
 
 static Exporter* e;
@@ -35,38 +54,33 @@ extern "C" void prom_setup(int port) {
 
 Exporter::Exporter(std::string bind_address) :
         exposer_(new Exposer(bind_address)),
-        registry_(std::make_shared<Registry>()) {
-    temperature_family_ = &BuildGauge()
+        registry_(std::make_shared<Registry>()),
+        updates_(AutoMetrics<Counter>(&BuildCounter()
+            .Name("updates")
+            .Help("count of device updates received")
+            .Register(*registry_))),
+        temperature_(&BuildGauge()
             .Name("temperature")
             .Help("temperature in device-reported degrees")
-            .Register(*registry_);
+            .Register(*registry_)) {
     exposer_->RegisterCollectable(registry_);
-}
-
-Gauge& Exporter::temperature(std::map<std::string, std::string> labels) {
-    auto combined = std::accumulate(
-        labels.begin(), labels.end(), std::string{},
-        [](const std::string& acc,
-           const std::pair<std::string, std::string>& label_pair) {
-          return acc + '|' + label_pair.first + '=' + label_pair.second;
-        });
-    auto search = temperatures_.find(combined);
-    if (search != temperatures_.end()) {
-        return *search->second;
-    }
-    Gauge& t = temperature_family_->Add(labels);
-    temperatures_.insert(std::make_pair(combined, &t));
-    return t;
 }
 
 extern "C" void prom_ambient_weather(const char *model, int device_id, int channel, int battery_low, double temperature, int humidity) {
     std::stringstream device_id_, channel_;
     device_id_ << device_id;
     channel_ << channel;
-    std::map<std::string, std::string> key({
+    std::map<std::string, std::string> labels({
         {"model", model},
         {"device_id", device_id_.str()},
         {"channel", channel_.str()}});
-    e->temperature(key).Set(temperature);
+    auto key = std::accumulate(
+        labels.begin(), labels.end(), std::string{},
+        [](const std::string& acc,
+           const std::pair<std::string, std::string>& label_pair) {
+          return acc + '|' + label_pair.first + '=' + label_pair.second;
+        });
+    e->updates_.get(key, labels).Increment();
+    e->temperature_.get(key, labels).Set(temperature);
 }
 }
